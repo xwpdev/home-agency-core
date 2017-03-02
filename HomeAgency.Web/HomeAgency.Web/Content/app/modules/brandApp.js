4fe6a1bc57803ca4a17648ae5ced9fb87f102715
xqpdev <charith.suriyakula@gmail.com>
2017-03-02 13:19:33 +0530

    UI updates

---
+++ HomeAgency.Web/HomeAgency.Web/Content/app/modules/brandApp.js
@@ -0,0 +1,23 @@
+﻿var brandApp = angular.module('brandApp', ['ui.bootstrap']);
+
+brandApp.controller('brandController', function ($scope, $uibModal) {
+    $scope.brandList = [];
+    $scope.loadData = function () {
+        $scope.brandList.push({ id: 1, name: "Delmage", active: false });
+    }
+    $scope.openAddPanel = function () {
+        $uibModal.open({
+            templateUrl: 'brandAddPanel',
+            backdrop: "static",
+            controller: ['$scope', '$uibModalInstance', function ($scopeChild, $uibModalInstance) {
+                $scopeChild.brand = { active: true };
+                $scopeChild.saveBrand = function () {
+                    console.log($scopeChild.brand);
+                }
+                $scopeChild.cancelModal = function () {
+                    $uibModalInstance.close()
+                }
+            }]
+        });
+    }
+});
