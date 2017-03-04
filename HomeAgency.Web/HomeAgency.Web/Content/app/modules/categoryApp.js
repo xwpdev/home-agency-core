9dcbd07353c05e62be19c7c0ce07488fc213d9f9
irangaRox <irangapathirane@gmail.com>
2017-03-05 04:08:56 +0530

    adds category

---
+++ HomeAgency.Web/HomeAgency.Web/Content/app/modules/categoryApp.js
@@ -0,0 +1,24 @@
+﻿var categoryApp = angular.module('categoryApp', ['ui.bootstrap']);
+
+categoryApp.controller('categoryController', function ($scope, $uibModal) {
+    $scope.categoryList = [];
+    $scope.loadData = function () {
+        $scope.categoryList.push({ id: 1, name: "Noodles", active: false });
+        $scope.categoryList.push({ id: 2, name: "Supiri Soya", active: false });
+    }
+    $scope.openAddPanel = function () {
+        $uibModal.open({
+            templateUrl: 'categoryAddPanel',
+            backdrop: "static",
+            controller: ['$scope', '$uibModalInstance', function ($scopeChild, $uibModalInstance) {
+                $scopeChild.category = { active: true };
+                $scopeChild.saveCategory = function () {
+                    console.log($scopeChild.category);
+                }
+                $scopeChild.cancelModal = function () {
+                    $uibModalInstance.close()
+                }
+            }]
+        });
+    }
+});
